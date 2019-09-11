24f5ad81a06a3fa9ad590085744dcf784d004d21
Andy Jones <hello@awj.digital>
2019-09-11 20:21:30 +0100

    updates to processing v3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
@@ -44,6 +45,7 @@ app.use('/public', express.static(path.join(__dirname, '/public')))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
